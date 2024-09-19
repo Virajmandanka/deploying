c1e11dbc9de64929d823d9691ecc6e2f8d961465
Virajmandanka <virajmandanka6@gmail.com>
2024-09-19 16:12:48 +0530

    db

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const jsonServer = require("server"); 
+const jsonServer = require("json-server"); 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3002; 
+const port = process.env.PORT || 3001; 
 
 server.use(middlewares);
 server.use(router);
